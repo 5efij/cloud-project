b02feb05359a22901d1049982d7d397694f0feda
5efij <mohammed.je120@gmail.com>
2026-03-14 02:12:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.getElementById("attacks").innerText = 1243;
-document.getElementById("phishing").innerText = 540;
-document.getElementById("malware").innerText = 320;
-document.getElementById("ddos").innerText = 383;
+var map = L.map('map').setView([20,0],2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Cyber Threat Map'
+}).addTo(map);
+
+var attacks = [
+{country:"USA",lat:37,lon:-95,attacks:320},
+{country:"Germany",lat:51,lon:10,attacks:150},
+{country:"Russia",lat:60,lon:90,attacks:500},
+{country:"China",lat:35,lon:105,attacks:420},
+{country:"Brazil",lat:-14,lon:-51,attacks:110}
+];
+
+attacks.forEach(a=>{
+L.circleMarker([a.lat,a.lon],{
+radius:8,
+color:"red"
+}).addTo(map)
+.bindPopup(a.country+"<br>Attacks: "+a.attacks);
+});
