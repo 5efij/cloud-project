4f637f8ca34ca07422f328c19933a540a0ce5eed
5efij <mohammed.je120@gmail.com>
2026-03-14 01:31:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("attacks").innerText = 1243;
+document.getElementById("phishing").innerText = 540;
+document.getElementById("malware").innerText = 320;
+document.getElementById("ddos").innerText = 383;
