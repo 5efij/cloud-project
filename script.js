63aa9173c9678bbef443f878fb03034f07962494
5efij <mohammed.je120@gmail.com>
2026-03-14 02:38:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 var map = L.map('map').setView([20,0],2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Cyber Threat Map'
+maxZoom:5
 }).addTo(map);
+
+var attacks = [
+{country:"Iraq",lat:33,lon:44,attacks:120},
+{country:"USA",lat:37,lon:-95,attacks:320},
+{country:"Germany",lat:51,lon:10,attacks:150},
+{country:"China",lat:35,lon:105,attacks:420},
+{country:"Brazil",lat:-14,lon:-51,attacks:110}
+];
+
+attacks.forEach(function(a){
+
+L.circleMarker([a.lat,a.lon],{
+radius:8,
+color:"red"
+}).addTo(map)
+
+.bindPopup("Country: "+a.country+"<br>Attacks: "+a.attacks);
+
+});
